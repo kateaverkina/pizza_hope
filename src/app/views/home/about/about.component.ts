94146af255d72596ab5b06716554c953361af6e8
kateaverkina <148697748+kateaverkina@users.noreply.github.com>
2024-11-19 20:08:54 +0300

    Add files via upload

---
+++ src/app/views/home/about/about.component.ts
@@ -0,0 +1,25 @@
+import {AfterViewInit, Component, OnInit, ViewChild} from '@angular/core';
+import {PopupComponent} from "../../../shared/components/popup/popup.component";
+import {NgbModal} from "@ng-bootstrap/ng-bootstrap";
+
+@Component({
+  selector: 'about',
+  templateUrl: './about.component.html',
+  styleUrls: ['./about.component.scss']
+})
+export class AboutComponent implements OnInit, AfterViewInit {
+
+  constructor(private modalService: NgbModal) { }
+
+  ngOnInit(): void {
+  }
+
+  @ViewChild(PopupComponent)
+  private popupComponent!: PopupComponent;
+  ngAfterViewInit() {
+    // this.modalService.open(this.popup, {});
+    this.popupComponent.open();
+  }
+
+
+}
